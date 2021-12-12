5a913b16d8970de7b174d72e9c7f705a73e1a6a8
Biswajeet Das <biswajeetdas18@gmail.com>
2021-12-12 21:02:58 +0530

    Update index.jsx

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -10,9 +10,9 @@ const NavbarLinks = ({ desktop }) => (
 		<AnchorLink className="contact" href="#contact">
 			Contact
 		</AnchorLink>
-		<a href={Resume} download="biswajeetdas">
-			Resume
-		</a>
+// 		<a href={Resume} download="biswajeetdas">
+// 			Resume
+// 		</a>
 	</Wrapper>
 )
 
